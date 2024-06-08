a259f6867886fdcb117ba9754095eb5bafca1d88
Ashutosh Shukla <ashutosh6471@gmail.com>
2024-06-08 23:01:31 +0530

    User joining the room logic and their record of joining being stored added

--- src/handlers/room-handler.ts
+++ src/handlers/room-handler.ts
@@ -1,17 +1,38 @@
 import { Socket } from "socket.io";
 import logger from "../../config/logger-config"; // Update the path to logger-config
 import { v4 as UUIDv4 } from 'uuid';
+import IRoomParams from "../interfaces/IRoomParams";
+
+const rooms: Record<string,string[]> = {};
 
 const roomHandler = (socket: Socket) => {
     const createRoom = () => {
         const roomId = UUIDv4();
         socket.join(roomId);
+
+        rooms[roomId] = [];
+
         socket.emit("room-created", { roomId });
         logger.info(`Room Created with ID: ${roomId}`);
     }
 
-    const joinedRoom = ( {roomId}: {roomId:string}) => {
-        logger.info(`New user has joined the room ${roomId}`);
+    const joinedRoom = ( {roomId,peerId}: IRoomParams) => {
+        if(rooms[roomId]){
+            logger.info(`New user has joined the room ${roomId} with peer id as ${peerId}`);
+            rooms[roomId].push(peerId);
+            logger.info(JSON.stringify(rooms));
+            socket.join(roomId);
+
+
+            socket.emit("get-users", {
+                roomId,
+                participants: rooms[roomId]
+
+            });
+
+
+        }
+
     }
 
     socket.on("create-room", createRoom);
